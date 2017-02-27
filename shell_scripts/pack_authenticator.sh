d3b3e8a8cb328a1f2260cc46676a86113ca9906e
Shankar <shankar21mail@gmail.com>
2017-02-27 12:20:54 +0530

    feat/authenticator_as_submodule: integrate authenticator as submodule
    Integrated SAFE Authenticator as git submodule and added build scripts for it.

---
+++ shell_scripts/pack_authenticator.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+git submodule update --init --recursive
+
+DEST_FOLD="../app/node_modules/beaker-plugin-safe-authenticator-1"
+
+cd authenticator
+
+git submodule update --init --recursive
+
+npm i && npm run build-native && npm run build 
+
+mkdir -p $DEST_FOLD
+
+FILES_ARR="./dist ./index.js ./package.json"
+
+for File in $FILES_ARR
+do
+	cp -fr $File $DEST_FOLD
+done
+
+echo "Packed Authenticator!"
