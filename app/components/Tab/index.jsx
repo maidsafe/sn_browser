73ef3a1a9a5fbec3ab4ed17bcefdc1da7157eb07
Josh Wilson <joshuef@gmail.com>
2017-04-19 09:40:19 +0200

    inspect element in webview now thanks

--- app/components/Tab/index.jsx
+++ app/components/Tab/index.jsx
@@ -115,17 +115,10 @@ export default class Tab extends Component {
                 label : 'Inspect element',
                 click : ( e ) =>
                 {
-                    remote.getCurrentWindow().inspectElement(rightClickPosition.x, rightClickPosition.y);
+                    wv.inspectElement(rightClickPosition.x, rightClickPosition.y);
                 }
             }
         ]);
-
-
-        // menu.append(new MenuItem({label: 'Copy', click() { console.log('item 1 clicked') }}))
-        // menu.append(new MenuItem({label: 'MenuItem1', click() { console.log('item 1 clicked') }}))
-        // menu.append(new MenuItem({type: 'separator'}))
-        // menu.append(new MenuItem({label: 'MenuItem2', type: 'checkbox', checked: true}))
-
 
 
         const callbackSetup = () => {
