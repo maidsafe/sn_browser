ce3279bea31ff2f02adfa716ebd0c3ea93673c48
Shankar <shankar21mail@gmail.com>
2017-06-02 16:57:44 +0530

    fix/rebuild: resolve rebuild after packaging authenticator
    Resolved running rebuild after pack-authenticator command on windows.

--- scripts/pack_authenticator.sh
+++ scripts/pack_authenticator.sh
@@ -3,19 +3,23 @@ clean=""
 if [ $1 ]; then
   clean="--clean"
 fi
-git submodule update --init --recursive
+#git submodule update --init --recursive
 
 DEST_FOLD="../app/node_modules/beaker-plugin-safe-authenticator"
 
 cd authenticator
 
-npm i && npm run build-libs -- --features="mock-routing" $clean && npm run build
+#npm i && npm run build-libs -- --features="mock-routing" $clean && npm run build
 
 mkdir -p $DEST_FOLD
 
-FILES_ARR="./dist ./index.js ./package.json"
+FILES_ARR="./dist ./index.js ./_package.json"
 
 for File in $FILES_ARR
 do
 	cp -fr $File $DEST_FOLD
 done
+
+mv $DEST_FOLD"/_package.json" $DEST_FOLD"/package.json"
+
+rm -rf _package.json
