b806488e78fd8fab5c5098a02a285600190086fd
bochaco <gabrielviganotti@gmail.com>
2017-06-26 16:44:57 +0900

    feat/Support network observer callbacks from main to renderer process

--- app/lib/fg/import-web-apis.js
+++ app/lib/fg/import-web-apis.js
@@ -4,7 +4,10 @@ import rpc from 'pauls-electron-rpc'
 // it would be better to import this from package.json
 const BEAKER_VERSION = '0.0.1'
 const WITH_CALLBACK_TYPE_PREFIX = '_with_cb_';
+const WITH_ASYNC_CALLBACK_TYPE_PREFIX = '_with_async_cb_';
 
+// Use a readable RPC stream to invoke a provided callback function,
+// resolving the promise upon the closure of the stream the sender.
 const readableToCallback = (rpcAPI) => {
   return (arg1, cb) => {
     return new Promise((resolve, reject) => {
@@ -12,6 +15,26 @@ const readableToCallback = (rpcAPI) => {
       r.on('data', data => cb.apply(cb, data));
       r.on('error', err => reject(err));
       r.on('end', () => resolve());
+    });
+  }
+}
+
+// Use a readable RPC stream to invoke a provided callback function even after
+// resolving the promise.
+const readableToAsyncCallback = (rpcAPI) => {
+  return (arg1, cb) => {
+    return new Promise((resolve, reject) => {
+      let firstValueReceived = false;
+      var r = rpcAPI(arg1);
+      r.on('data', data => {
+        if (!firstValueReceived) {
+          firstValueReceived = true;
+          resolve(data[0]);
+        } else {
+          cb.apply(cb, data);
+        }
+      });
+      r.on('error', err => reject(err));
     });
   }
 }
@@ -28,20 +51,28 @@ export default function () {
 
     let fnsToImport = [];
     let fnsWithCallback = [];
+    let fnsWithAsyncCallback = [];
     for (var fn in webAPIs[k]) {
       // We adapt the functions which contain a callback
       if (fn.startsWith(WITH_CALLBACK_TYPE_PREFIX)) {
-        // We use a readable type to receive the data from rpc channel
+        // We use a readable type to receive the data from the RPC channel
         let manifest = {[fn]: 'readable'};
         let rpcAPI = rpc.importAPI(WITH_CALLBACK_TYPE_PREFIX + k, manifest, { timeout: false })
         // We expose the function removing the WITH_CALLBACK_TYPE_PREFIX prefix
         let newFnName = fn.replace(WITH_CALLBACK_TYPE_PREFIX, '');
         fnsWithCallback[newFnName] = readableToCallback(rpcAPI[fn]);
+      } else if (fn.startsWith(WITH_ASYNC_CALLBACK_TYPE_PREFIX)) {
+        // We use a readable type to receive the data from the RPC channel
+        let manifest = {[fn]: 'readable'};
+        let rpcAPI = rpc.importAPI(WITH_ASYNC_CALLBACK_TYPE_PREFIX + k, manifest, { timeout: false })
+        // We expose the function removing the WITH_ASYNC_CALLBACK_TYPE_PREFIX prefix
+        let newFnName = fn.replace(WITH_ASYNC_CALLBACK_TYPE_PREFIX, '');
+        fnsWithAsyncCallback[newFnName] = readableToAsyncCallback(rpcAPI[fn]);
       } else {
         fnsToImport[fn] = webAPIs[k][fn];
       }
     }
 
-    window[k] = Object.assign(rpc.importAPI(k, fnsToImport, { timeout: false }), fnsWithCallback);
+    window[k] = Object.assign(rpc.importAPI(k, fnsToImport, { timeout: false }), fnsWithCallback, fnsWithAsyncCallback);
   }
 }
