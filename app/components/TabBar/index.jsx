0b0f63fd1bbc61195925ef88eb7161e577814af9
Josh Wilson <joshuef@gmail.com>
2017-07-23 18:33:03 +0200

    udpated some packages

--- app/components/TabBar/index.jsx
+++ app/components/TabBar/index.jsx
@@ -1,13 +1,13 @@
 // @flow
-import React, { Component }     from 'react';
-import PropTypes                from 'prop-types';
-import { ipcRenderer, ipcMain }          from 'electron';
-import styles                   from './tabBar.css';
-import MdClose                  from 'react-icons/lib/md/close';
-import MdAdd                    from 'react-icons/lib/md/add';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { ipcRenderer, ipcMain } from 'electron';
+import styles from './tabBar.css';
+import MdClose from 'react-icons/lib/md/close';
+import MdAdd from 'react-icons/lib/md/add';
 
-export default class TabBar extends Component {
-
+export default class TabBar extends Component
+{
     static defaultProps =
     {
         tabInFocus : 0
@@ -23,8 +23,8 @@ export default class TabBar extends Component {
         super( props );
         this.state =
         {
-            tabInFocus : 0 //to update when many tabs can exist
-        }
+            tabInFocus : 0 // to update when many tabs can exist
+        };
     }
 
 
@@ -43,7 +43,6 @@ export default class TabBar extends Component {
         const { closeTab } = this.props;
 
         closeTab( { index: tabData.key } );
-
     }
 
     handleAddTabClick( event )
@@ -60,38 +59,42 @@ export default class TabBar extends Component {
 
     render()
     {
-
         const { tabs } = this.props;
 
         return (
-            <div className={styles.container}>
-                <div  className={styles.dragZone}  />
-                <div className={styles.tabBar}>
+            <div className={ styles.container }>
+                <div className={ styles.dragZone } />
+                <div className={ styles.tabBar }>
                     {
                         tabs.map( ( tab, i ) =>
                         {
-                            if( tab.get( 'isClosed') )
+                            if ( tab.get( 'isClosed' ) )
+                            {
                                 return;
+                            }
 
-                            let isActiveTab = tab.get('isActiveTab')
-                            let tabStyleClass  = styles.tab;
-                            let tabData     = { key: i, url: tab.get('url') };
-                            if( isActiveTab )
+                            const isActiveTab = tab.get( 'isActiveTab' );
+                            let tabStyleClass = styles.tab;
+                            const tabData = { key: i, url: tab.get( 'url' ) };
+                            if ( isActiveTab )
                             {
                                 tabStyleClass = styles.activeTab;
                             }
-                            return (<div
-                                        key={ i }
-                                        className={ tabStyleClass }
-                                        onClick={ this.handleTabClick.bind(this, tabData  ) }>
-                                            <span className={ styles.tabText }>{ tab.get('title') || 'New Tab' }</span>
-                                            <MdClose className={ styles.tabCloseButton }
-                                                 onClick={ this.handleTabClose.bind( this, tabData  ) } />
-                                    </div>)
-                        })
+                            return ( <div
+                                key={ i }
+                                className={ tabStyleClass }
+                                onClick={ this.handleTabClick.bind( this, tabData ) }
+                            >
+                                <span className={ styles.tabText }>{ tab.get( 'title' ) || 'New Tab' }</span>
+                                <MdClose
+                                    className={ styles.tabCloseButton }
+                                    onClick={ this.handleTabClose.bind( this, tabData ) }
+                                />
+                            </div> );
+                        } )
                     }
-                    <div className={ styles.addTab } onClick={ this.handleAddTabClick.bind(this) }>
-                        <div className={styles.tabBox}>
+                    <div className={ styles.addTab } onClick={ this.handleAddTabClick.bind( this ) }>
+                        <div className={ styles.tabBox }>
                             <MdAdd className={ styles.tabAddButton } />
                         </div>
                     </div>
