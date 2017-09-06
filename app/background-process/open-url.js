d0f07ee256de8f0cb0d57a3ecdcea4c0b2e949c1
Gabriel Viganotti <bochaco@users.noreply.github.com>
2017-09-06 19:10:34 -0300

    Revert "MAID-2308 Prevent destroyed object warnings with openURL (#153)"
    This reverts commit 1a8b7a35b430272e5a30b5bada9f9a08c1af9ec5.

--- app/background-process/open-url.js
+++ app/background-process/open-url.js
@@ -1,46 +1,20 @@
 // handle OSX open-url event
 import { ipcMain } from 'electron'
-import { createShellWindow } from './ui/windows'
-
 var queue = []
 var commandReceiver
-
-function runQueue()
-{
-  queue.forEach(url => commandReceiver.send('command', 'file:new-tab', url))
-  queue.length = 0
-}
 
 export function setup () {
   ipcMain.once('shell-window-ready', function (e) {
     commandReceiver = e.sender
-    runQueue()
+    queue.forEach(url => commandReceiver.send('command', 'file:new-tab', url))
+    queue.length = 0
   })
 }
 
-
-export function unsetReceiver()
-{
-  commandReceiver = null;
-  return;
-}
-
-export function updateReceiver( webContents )
-{
-  console.log("trying to update to be", webContents.id);
-  commandReceiver = webContents;
-  runQueue()
-}
-
 export function open (url) {
-
-  if (commandReceiver !== null) {
+  if (commandReceiver) {
     commandReceiver.send('command', 'file:new-tab', url)
   } else {
     queue.push(url)
-  }
-
-  if( url.startsWith('safe-') ) {
-    createShellWindow()
   }
 }
