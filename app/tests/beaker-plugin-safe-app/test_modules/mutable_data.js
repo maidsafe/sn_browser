65e66d31e8afd12db0e8e0e8b4d2ff80d9f208cc
hunterlester <guilfordhunter@gmail.com>
2017-09-20 16:59:19 -0700

    adds newPrivate test to safeMutableData

--- app/tests/beaker-plugin-safe-app/test_modules/mutable_data.js
+++ app/tests/beaker-plugin-safe-app/test_modules/mutable_data.js
@@ -3,6 +3,12 @@ let testHelpers = require('./helpers');
 
 describe('window.safeMutableData', () => {
   const data = 'piece of information to be encrypted';
+  it('creates a private, randomly named, mutable data structure that can encrypt it\'s entries', async () => {
+    const privateMDHandle = await testHelpers.createRandomPrivateMutableData();
+    const encryptedValue = await window.safeMutableData.encryptValue(privateMDHandle, data);
+    const humanReadableString = String.fromCharCode.apply(null, new Uint8Array(encryptedValue));
+    should(humanReadableString).not.equal(data);
+  });
 
   it('creates a public, randomly named, mutable data that cannot encrypt it\'s entries', async () => {
     const publicMDHandle = await testHelpers.createRandomPublicMutableData();
@@ -11,14 +17,28 @@ describe('window.safeMutableData', () => {
     should(humanReadableString).be.equal(data);
   });
 
-  it('creates a private, randomly named, mutable data structure that can encrypt it\'s entries', async () => {
-    const privateMDHandle = await testHelpers.createRandomPrivateMutableData();
-    const encryptedValue = await window.safeMutableData.encryptValue(privateMDHandle, data);
-    const humanReadableString = String.fromCharCode.apply(null, new Uint8Array(encryptedValue));
-    should(humanReadableString).not.equal(data);
+  it('creates a private Mutable Data that can be custom-defined and encrypted', async () => {
+    const customName = testHelpers.createRandomXorName();
+    const appHandle = await testHelpers.authoriseAndConnect();
+    const nonce = await window.safeCrypto.generateNonce(appHandle);
+    const encKeyPairHandle = await window.safeCrypto.generateEncKeyPair(appHandle);
+    const secEncKeyHandle = await window.safeCryptoKeyPair.getSecEncKey(encKeyPairHandle);
+    const rawSecEncKey = await window.safeCryptoSecEncKey.getRaw(secEncKeyHandle);
+    const mdHandle = await window.safeMutableData.newPrivate(appHandle, customName, testHelpers.TAG_TYPE_DNS, rawSecEncKey.buffer, nonce.buffer);
+    const encryptedKey = await window.safeMutableData.encryptKey(mdHandle, 'encrypted_key_1');
+    const encryptedValue = await window.safeMutableData.encryptValue(mdHandle, 'encrypted_value_2');
+    let entry = {};
+    entry[String.fromCharCode.apply(null, new Uint8Array(encryptedKey))] = new Uint8Array(encryptedValue);
+    await window.safeMutableData.quickSetup(mdHandle, entry);
+    const retrievedValue = await window.safeMutableData.get(mdHandle, String.fromCharCode.apply(null, new Uint8Array(encryptedKey)));
+    should(String.fromCharCode.apply(null, retrievedValue.buf))
+    .not.equal('encrypted_value_2');
+    const deciphered = await window.safeMutableData.decrypt(mdHandle, retrievedValue.buf);
+    should(String.fromCharCode.apply(null, new Uint8Array(deciphered)))
+    .be.equal('encrypted_value_2');
   });
 
-  it('creates a public Mutable Data, taking an argument to custom-define it\'s name', async () => {
+  it('creates a public Mutable Data that can be custom-defined', async () => {
     const customName = testHelpers.createRandomXorName();
     const appHandle = await testHelpers.authoriseAndConnect();
     const mdHandle = await window.safeMutableData.newPublic(appHandle, customName, testHelpers.TAG_TYPE_DNS);
