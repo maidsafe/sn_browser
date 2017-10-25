75a94959d18520ef71f8f23e0c391b85a4195bd0
joshuef <joshuef@gmail.com>
2017-10-25 19:57:09 +0200

    MAID-2394 feat/Reconnect to safe:// (#214)
    * feat/logging: Pass log in render to protocol registering plugins
    * feat/reconnect: Add disconnect messagebar + autoreconnect
    * feat/reconnect: Check app connection before triggering reconnect
    * fix/browserState: hack to check browserstate as returned before reconnecting

--- app/background-process/safe-storage/helpers.js
+++ app/background-process/safe-storage/helpers.js
@@ -1,10 +1,29 @@
 import beakerSafePlugin from 'beaker-plugin-safe-app';
+import beakerAuthPlugin  from 'beaker-plugin-safe-authenticator';
 
 const allAPIs = beakerSafePlugin.webAPIs;
+const authAPIs = beakerAuthPlugin.webAPIs;
 
 const WITH_CALLBACK_TYPE_PREFIX = '_with_cb_';
 const WITH_ASYNC_CALLBACK_TYPE_PREFIX = '_with_async_cb_';
 
+export const authReconnect = ( ) =>
+{
+  const authApi = authAPIs.find( api =>  api.name === 'safeAuthenticator' )
+
+  let reconnect = authApi.methods.reconnect;
+
+  reconnect();
+}
+
+export const getAuthenticatorStatus = ( ) =>
+{
+  const authApi = authAPIs.find( api =>  api.name === 'safeAuthenticator' )
+
+  let getNetworkState = authApi.methods.getNetworkState;
+
+  getNetworkState();
+}
 
 export const getAPI = ( name ) =>
 {
