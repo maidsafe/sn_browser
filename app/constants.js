515ad9e9ee1b64e0d9d7e114cf9f896571965f71
bochaco <gabrielviganotti@gmail.com>
2017-12-26 19:17:07 -0300

    fix/Pass system URI's exec path as an array of arguments

--- app/constants.js
+++ app/constants.js
@@ -69,7 +69,7 @@ const execPath = ( ) =>
         return '';
     }
 
-    return isRunningUnpacked ? `${process.execPath} ${app.getAppPath()}` : app.getPath( 'exe' );
+    return isRunningUnpacked ? [ process.execPath, app.getAppPath() ] : [ app.getPath( 'exe' ) ];
 };
 
 const appInfo = {
@@ -78,7 +78,6 @@ const appInfo = {
         scope          : null,
         name           : pkg.productName,
         vendor         : pkg.author.name,
-        // customSearchPath : isRunningUnpacked ? process.execPath : app.getPath( 'exe' )
         customExecPath : execPath()
     },
     opt : {
