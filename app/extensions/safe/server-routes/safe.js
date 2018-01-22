2a21a9e5d5e5d5e1b2ba906dc5c4418bf498123e
Josh Wilson <joshuef@gmail.com>
2018-01-22 12:29:35 +0100

    feat/partial-content: Separate out partial content requests.
    Pass on range headers to webFetch. Reply with partial content headers etc.

--- app/extensions/safe/server-routes/safe.js
+++ app/extensions/safe/server-routes/safe.js
@@ -11,6 +11,12 @@ const safeRoute = {
         {
             const link = `safe://${request.params.link}`;
             const app = getAppObj();
+            const headers = request.headers;
+            let isRangeReq = false;
+            const BYTES = 'bytes=';
+
+            let start;
+            let end;
 
             logger.verbose( `Handling SAFE req: ${link}` );
 
@@ -19,11 +25,52 @@ const safeRoute = {
                 return reply( 'SAFE not connected yet' );
             }
 
-            const data = await app.webFetch( link );
+            if ( headers.range )
+            {
+                const range = headers.range;
+                const rangeArray =
+                  range.substring( BYTES.length, range.length )
+                      .replace( /['"]+/g, '' )
+                      .split( '-' );
+
+                start = rangeArray[0] ? parseInt( rangeArray[0], 10 ) : null;
+                end = rangeArray[1] ? parseInt( rangeArray[1], 10 ) : null;
+
+                //we need to separate out 0- length requests which are not partial content
+                if( start && start !== 0 )
+                {
+                    //should have a start !== 0
+                    isRangeReq = true;
+                }
+
+                if( start === 0 && end )
+                {
+                    isRangeReq = true;
+                }
+            }
+
+            // setup opts object
+            const options = { headers };
+
+            if ( isRangeReq )
+            {
+                options.range = { start, end };
+            }
+
+            const data = await app.webFetch( link, options );
+
+            if ( headers.range )
+            {
+                return reply( data.body )
+                    .code( 206 )
+                    .type( data.headers['Content-Type'] )
+                    .header( 'Content-Range', data.headers['Content-Range'] )
+                    .header( 'Content-Length', data.headers['Content-Length'] );
+            }
 
             return reply( data.body )
                 .type( data.headers['Content-Type'] )
-                .header( 'Content-Encoding', 'chunked');
+                .header( 'Content-Encoding', 'chunked' );
         }
         catch ( e )
         {
@@ -31,7 +78,7 @@ const safeRoute = {
             return reply( e.message || e );
         }
     }
-}
+};
 
 
 export default safeRoute;
