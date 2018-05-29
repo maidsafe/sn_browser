f03116cbd410741e7a86b6b9858f66b27a0ae6c6
Josh Wilson <joshuef@gmail.com>
2018-05-29 09:13:00 +0200

    hack/tests: actually exclude weakref test on windows now

--- test/runTests.js
+++ test/runTests.js
@@ -1,6 +1,12 @@
 const spawn = require( 'cross-spawn' );
 const path = require( 'path' );
 
+
+const platform = process.platform;
+const OSX = 'darwin';
+const LINUX = 'linux';
+const WINDOWS = 'win32';
+
 const s = `\\${path.sep}`;
 let pattern;
 const arg = process.argv[2];
@@ -28,6 +34,12 @@ switch ( arg )
     default :
     {
         pattern = `test${s}(?!e2e${s})[^${s}]+${s}.+\\.spec\\.js$`;
+
+        if ( platform === WINDOWS )
+        {
+            //exclude weakref tests for now.
+            pattern = `test${s}(?!e2e${s})[^${s}]+${s}(?!setupPreloadAPIs).+\\.spec\\.js$`;
+        }
     }
 }
 
