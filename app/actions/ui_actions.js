f0dfa1a072ae87ef32ddddec2f5bc175bead518c
hunterlester <guilfordhunter@gmail.com>
2018-07-05 15:44:07 -0700

    MAID-2697 feat/ui: adds indication of webview loading

--- app/actions/ui_actions.js
+++ app/actions/ui_actions.js
@@ -2,11 +2,11 @@ import { createActions } from 'redux-actions';
 
 export const TYPES = {
     DESELECT_ADDRESS_BAR : 'DESELECT_ADDRESS_BAR',
-    SELECT_ADDRESS_BAR : 'SELECT_ADDRESS_BAR',
-    BLUR_ADDRESS_BAR  : 'BLUR_ADDRESS_BAR',
-    RESET_STORE : 'RESET_STORE',
-    RELOAD_PAGE : 'RELOAD_PAGE',
-    PAGE_LOADED : 'PAGE_LOADED'
+    SELECT_ADDRESS_BAR   : 'SELECT_ADDRESS_BAR',
+    BLUR_ADDRESS_BAR     : 'BLUR_ADDRESS_BAR',
+    RESET_STORE          : 'RESET_STORE',
+    RELOAD_PAGE          : 'RELOAD_PAGE',
+    PAGE_LOADED          : 'PAGE_LOADED'
 };
 
 export const {
