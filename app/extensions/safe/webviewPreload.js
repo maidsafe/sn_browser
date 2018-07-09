5be12ff3214720d7fb2065b008737badba8af8fb
Josh Wilson <joshuef@gmail.com>
2018-07-09 09:04:07 +0100

    feat/webId: Send only #me to webviews

--- app/extensions/safe/webviewPreload.js
+++ app/extensions/safe/webviewPreload.js
@@ -26,7 +26,13 @@ const webIdEventEmitter = new WebIdEvents();
 const getCurrentWebId = ( webIds ) =>
 {
     const webId = webIds.find( webId => webId.isSelected );
-    return webId;
+
+    //assuming #me for now.
+    if( webId && webId['#me'] ) return webId['#me'];
+
+
+    // TODO: Should return error if non set?
+    return {};
 }
 
 
