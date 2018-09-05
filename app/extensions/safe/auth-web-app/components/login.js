fb13b975dbed336459de3dcafc35753f509700ea
Josh Wilson <joshuef@gmail.com>
2018-09-05 11:12:02 +0200

    lint/auth-app: Lint auth app create account and login pages

--- app/extensions/safe/auth-web-app/components/login.js
+++ app/extensions/safe/auth-web-app/components/login.js
@@ -2,163 +2,187 @@ import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { I18n } from 'react-redux-i18n';
 import classNames from 'classnames';
+import { AUTH_UI_CLASSES } from 'extensions/safe/auth-web-app/classes';
 
 import CardLoaderFull from './card_loader_full';
 import Popup from './popup';
 
-export default class Login extends Component {
+export default class Login extends Component
+{
   static propTypes = {
-    isAuthorised: PropTypes.bool,
-    libErrPopup: PropTypes.bool,
-    loading: PropTypes.bool,
-    error: PropTypes.string,
-    login: PropTypes.func,
-    clearError: PropTypes.func
+      isAuthorised : PropTypes.bool,
+      libErrPopup  : PropTypes.bool,
+      loading      : PropTypes.bool,
+      error        : PropTypes.string,
+      login        : PropTypes.func,
+      clearError   : PropTypes.func
   };
 
   static contextTypes = {
-    router: PropTypes.object.isRequired
+      router : PropTypes.object.isRequired
   };
 
-  constructor() {
-    super();
-    this.title = 'Sign in to manage your apps';
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.state = {
-      libErrPopup: false,
-      libErr: true
-    };
+  constructor()
+  {
+      super();
+      this.title = 'Sign in to manage your apps';
+      this.handleSubmit = this.handleSubmit.bind( this );
+      this.state = {
+          libErrPopup : false,
+          libErr      : true
+      };
   }
 
-  componentWillMount() {
-    if (this.props.isAuthorised) {
-      return this.props.push('/');
-    }
-    if (this.props.error) {
-      this.props.clearError();
-    }
-    this.setState({ libErrPopup: this.props.libErrPopup });
+  componentWillMount()
+  {
+      if ( this.props.isAuthorised )
+      {
+          return this.props.push( '/' );
+      }
+      if ( this.props.error )
+      {
+          this.props.clearError();
+      }
+      this.setState( { libErrPopup: this.props.libErrPopup } );
   }
 
-  componentDidMount() {
-    setTimeout(() => {
-      this.secretEle.focus();
-    }, 100);
+  componentDidMount()
+  {
+      setTimeout( () =>
+      {
+          this.secretEle.focus();
+      }, 100 );
   }
 
-  componentWillUpdate(nextProps) {
-    if (nextProps.isAuthorised) {
-      return this.props.push('/');
-    }
+  componentWillUpdate( nextProps )
+  {
+      if ( nextProps.isAuthorised )
+      {
+          return this.props.push( '/' );
+      }
   }
 
-  togglePassword(e) {
-    const input = e.target.parentElement.childNodes.item('input');
-    if (!(input && input.value)) {
-      return;
-    }
-    input.type = (input.type === 'text') ? 'password' : 'text';
+  togglePassword( e )
+  {
+      const input = e.target.parentElement.childNodes.item( 'input' );
+      if ( !( input && input.value ) )
+      {
+          return;
+      }
+      input.type = ( input.type === 'text' ) ? 'password' : 'text';
   }
 
-  handleSubmit(e) {
-    e.preventDefault();
-    const { login, clearError } = this.props;
-    clearError();
-    const secret = this.secretEle.value.trim();
-    const password = this.passwordEle.value.trim();
-    if (!secret || !password) {
-      return;
-    }
-    login(secret, password);
+  handleSubmit( e )
+  {
+      e.preventDefault();
+      const { login, clearError } = this.props;
+      clearError();
+      const secret = this.secretEle.value.trim();
+      const password = this.passwordEle.value.trim();
+      if ( !secret || !password )
+      {
+          return;
+      }
+      login( secret, password );
   }
 
-  render() {
-    const { error } = this.props;
+  render()
+  {
+      const { error } = this.props;
 
-    // if (loading) {
-    //   return <AuthLoader cancelAuthReq={clearAuthLoader} />;
-    // }
+      // if (loading) {
+      //   return <AuthLoader cancelAuthReq={clearAuthLoader} />;
+      // }
 
-    return (
-      <div>
-        <div className="card-main-b">
-          <div className="card-main-h">{this.title}</div>
-          <div className="card-main-cntr">
-            <Popup
-              show={this.state.libErrPopup}
-              error={this.state.libErr}
-              callback={() => {
-                this.setState({ libErrPopup: false });
-              }}
-              title={I18n.t('messages.failed_to_load_lib_title')}
-              desc={I18n.t('messages.failed_to_load_lib')}
-            />
-            {this.props.loading &&
-            <CardLoaderFull msg="Signing in, please wait!">{''}</CardLoaderFull>
-            }
-            <div className="auth">
-              <div className="auth-b login-b">
-                <div className="auth-form">
-                  <form onSubmit={this.handleSubmit}>
-                    <div className="inp-grp">
-                      <input
-                        type="password"
-                        id="acc-secret"
-                        name="acc-secret"
-                        ref={(c) => { this.secretEle = c; }}
-                        required
+      return (
+          <div>
+              <div className="card-main-b">
+                  <div className="card-main-h">{this.title}</div>
+                  <div className="card-main-cntr">
+                      <Popup
+                          show={ this.state.libErrPopup }
+                          error={ this.state.libErr }
+                          callback={ () =>
+                          {
+                              this.setState( { libErrPopup: false } );
+                          } }
+                          title={ I18n.t( 'messages.failed_to_load_lib_title' ) }
+                          desc={ I18n.t( 'messages.failed_to_load_lib' ) }
                       />
-                      <label htmlFor="acc-secret">Account Secret</label>
-                      <span className="msg error">{ error }</span>
-                      <button
-                        type="button"
-                        tabIndex="-1"
-                        className="eye-btn"
-                        onClick={this.togglePassword}
-                      >{' '}</button>
-                    </div>
-                    <div className="inp-grp">
-                      <input
-                        type="password"
-                        id="acc-password"
-                        name="acc-password"
-                        ref={(c) => { this.passwordEle = c; }}
-                        required
-                      />
-                      <label htmlFor="acc-password">Account Password</label>
-                      <button
-                        type="button"
-                        tabIndex="-1"
-                        className="eye-btn"
-                        onClick={this.togglePassword}
-                      >{' '}</button>
-                    </div>
-                    <div className="btn-grp">
-                      <button
-                        type="submit"
-                        className="btn primary long"
-                        disabled={this.props.libErrPopup}
-                      >Log in</button>
-                    </div>
-                  </form>
-                </div>
+                      {this.props.loading &&
+                      <CardLoaderFull msg="Signing in, please wait!">{''}</CardLoaderFull>
+                      }
+                      <div className="auth">
+                          <div className="auth-b login-b">
+                              <div className="auth-form">
+                                  <form onSubmit={ this.handleSubmit }>
+                                      <div className="inp-grp">
+                                          <input
+                                              type="password"
+                                              id="acc-secret"
+                                              name="acc-secret"
+                                              ref={ ( c ) =>
+                                              {
+                                                  this.secretEle = c;
+                                              } }
+                                              required
+                                          />
+                                          <label htmlFor="acc-secret">Account Secret</label>
+                                          <span className="msg error">{ error }</span>
+                                          <button
+                                              type="button"
+                                              tabIndex="-1"
+                                              className="eye-btn"
+                                              onClick={ this.togglePassword }
+                                          >{' '}</button>
+                                      </div>
+                                      <div className="inp-grp">
+                                          <input
+                                              type="password"
+                                              id="acc-password"
+                                              name="acc-password"
+                                              ref={ ( c ) =>
+                                              {
+                                                  this.passwordEle = c;
+                                              } }
+                                              required
+                                          />
+                                          <label htmlFor="acc-password">Account Password</label>
+                                          <button
+                                              type="button"
+                                              tabIndex="-1"
+                                              className="eye-btn"
+                                              onClick={ this.togglePassword }
+                                          >{' '}</button>
+                                      </div>
+                                      <div className="btn-grp">
+                                          <button
+                                              type="submit"
+                                              className="btn primary long"
+                                              disabled={ this.props.libErrPopup }
+                                          >Log in</button>
+                                      </div>
+                                  </form>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
               </div>
-            </div>
-          </div>
-        </div>
-        <div className="card-f">
+              <div className="card-f">
           Don&lsquo;t have an account? <a
-            className={classNames({ disabled: this.props.loading || this.props.libErrPopup })}
-            onClick={(e) => {
-              e.preventDefault();
-              if (this.props.loading || this.props.libErrPopup) {
-                return;
-              }
-              return this.props.push('/create-account');
-            }}
+              className={ classNames( { disabled: this.props.loading || this.props.libErrPopup } ) }
+              onClick={ ( e ) =>
+                      {
+                          e.preventDefault();
+                          if ( this.props.loading || this.props.libErrPopup )
+                          {
+                              return;
+                          }
+                          return this.props.push( '/create-account' );
+                      } }
           >CREATE ACCOUNT</a>
-        </div>
-      </div>
-    );
+              </div>
+          </div>
+      );
   }
 }
