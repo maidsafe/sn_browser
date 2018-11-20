f049a98b1d733b17d054e7ec59cf6c8194d854b9
Josh Wilson <joshuef@gmail.com>
2018-11-20 16:55:32 +0100

    Fix(UI); remove ugly mock wrapper

--- app/extensions/safe/components/wrapBrowser.js
+++ app/extensions/safe/components/wrapBrowser.js
@@ -5,33 +5,29 @@ import { SAFE } from 'extensions/safe/constants';
 
 // jsx css as babel not currently parsing css files here.
 const spectronAreaButton = {
-    width: '1px',
-    height:'10px',
-    display: 'inline-block',
-}
+    width   : '1px',
+    height  : '10px',
+    display : 'inline-block',
+};
 
 const spectronArea = {
-    backgroundColor: 'blue',
-    display: 'block',
-    width: '100%',
-    height: '10px'
-}
+    backgroundColor : 'blue',
+    display         : 'block',
+    width           : '100%',
+    height          : '10px'
+};
 
 const browserContainer = {
-    textAlign:       'center',
-    height:           '100%',
-    display:          'flex',
-    flexDirection:   'column',
-    position:         'relative'
-}
+    textAlign     : 'center',
+    height        : '100%',
+    display       : 'flex',
+    flexDirection : 'column',
+    position      : 'relative'
+};
 
-export const wrapBrowser = ( BrowserComponent, extensionFunctionality = {} ) =>
-{
-    return class wrappedSafeBrowser extends Component {
-        constructor(props) {
-            super(props);
-      }
-
+const wrapBrowser = ( BrowserComponent, extensionFunctionality = {} ) =>
+    class wrappedSafeBrowser extends Component
+    {
       static defaultProps =
       {
           addressBarIsSelected : false,
@@ -55,12 +51,12 @@ export const wrapBrowser = ( BrowserComponent, extensionFunctionality = {} ) =>
           setReadConfigStatus( SAFE.READ_STATUS.TO_READ );
       }
 
-      render() {
+      render()
+      {
           const { safeBrowserApp } = this.props;
-          const isMock = safeBrowserApp ? safeBrowserApp.isMock : false;
 
           return (
-              <div style={browserContainer}>
+              <div style={ browserContainer }>
                   {
                       isRunningSpectronTestProcess &&
                       <div
@@ -69,25 +65,22 @@ export const wrapBrowser = ( BrowserComponent, extensionFunctionality = {} ) =>
                           style={ spectronArea }
                       >
                           <button
-                              style={spectronAreaButton}
+                              style={ spectronAreaButton }
                               className={ `${CLASSES.SPECTRON_AREA__SPOOF_SAVE}` }
                               onClick={ this.handleSpectronTestSaveState }
                           />
                           <button
-                              style={spectronAreaButton}
+                              style={ spectronAreaButton }
                               className={ `${CLASSES.SPECTRON_AREA__SPOOF_LOAD}` }
                               onClick={ this.handleSpectronTestReadState }
                           />
                       </div>
 
                   }
-                  {
-                      isMock &&
-                      <span style={ { "WebkitAppRegion": "drag" } }>Running on a mock network</span>
-                  }
-                  <BrowserComponent {...this.props}/>
+                  <BrowserComponent { ...this.props } />
               </div>
-          )
+          );
       }
-    }
-}
+    };
+
+export default wrapBrowser;
