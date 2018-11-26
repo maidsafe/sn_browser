ae5e5e42802434710ea759c9c5083176bf50920e
Hunter Lester <guilfordhunter@gmail.com>
2018-11-26 14:14:39 -0800

    fix(memory): mocks safeBrowserApplication_actions to prevent call stack size error

--- __tests__/components/AddressBarInput.spec.js
+++ __tests__/components/AddressBarInput.spec.js
@@ -7,6 +7,7 @@ import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 const mockStore = configureStore();
 
+jest.mock( 'extensions/safe/actions/safeBrowserApplication_actions' );
 
 describe( 'AddressBarInput', () =>
 {
