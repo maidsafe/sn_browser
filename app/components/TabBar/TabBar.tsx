df5a2ccf04f029656c91766c4a8b0d6ef2234bc3
Josh Wilson <joshuef@gmail.com>
2019-04-29 17:55:18 +0200

    fix(TabBar): Prevents long tabtext overflow

--- app/components/TabBar/TabBar.tsx
+++ app/components/TabBar/TabBar.tsx
@@ -40,79 +40,86 @@ export class TabBar extends Component<TabBarProps> {
 
     getTabs = () => {
         const { tabs } = this.props;
-        return tabs.map( ( tab ) : React.ReactNode => {
-            let { title } = tab;
-            if ( isInternalPage( tab ) ) {
-                // TODO: DRY this out with TabContents.jsx
-                const urlObj = url.parse( tab.url );
-                switch ( urlObj.host ) {
-                    case INTERNAL_PAGES.HISTORY: {
-                        title = 'History';
-                        break;
-                    }
-                    case INTERNAL_PAGES.BOOKMARKS: {
-                        title = 'Bookmarks';
-                        break;
-                    }
-                    default: {
-                        title = null;
-                        break;
+        return tabs.map(
+            ( tab ): React.ReactNode => {
+                let { title } = tab;
+                if ( isInternalPage( tab ) ) {
+                    // TODO: DRY this out with TabContents.jsx
+                    const urlObj = url.parse( tab.url );
+                    switch ( urlObj.host ) {
+                        case INTERNAL_PAGES.HISTORY: {
+                            title = 'History';
+                            break;
+                        }
+                        case INTERNAL_PAGES.BOOKMARKS: {
+                            title = 'Bookmarks';
+                            break;
+                        }
+                        default: {
+                            title = null;
+                            break;
+                        }
                     }
                 }
+                if ( tab.isClosed ) {
+                    return;
+                }
+                const { isActiveTab } = tab;
+                let tabStyleClass = styles.tab;
+                const tabData = {
+                    key: tab.index,
+                    tabIndex: tab.index,
+                    url: tab.url
+                };
+                if ( isActiveTab ) {
+                    tabStyleClass = `${styles.activeTab} ${CLASSES.ACTIVE_TAB}`;
+                }
+                return (
+                    <div
+                        key={tab.index}
+                        className={`${tabStyleClass} ${CLASSES.TAB}`}
+                        onClick={( event ) => {
+                            this.handleTabClick( tabData, event );
+                        }}
+                    >
+                        <Row
+                            className={styles.tabRow}
+                            align="middle"
+                            justify="space-between"
+                            type="flex"
+                        >
+                            <Col>
+                                <div className={styles.faviconContainer}>
+                                    {tab.isLoading && (
+                                        <Icon type="loading" className={styles.loadingIcon} />
+                                    )}
+                                    {!tab.isLoading && tab.favicon && (
+                                        <img
+                                            alt=""
+                                            className={styles.favicon}
+                                            id="favicon-img"
+                                            src={tab.favicon}
+                                        />
+                                    )}
+                                </div>
+                            </Col>
+                            <Col className={styles.tabText}>{title || 'New Tab'}</Col>
+                            <Col>
+                                <Icon
+                                    className={`${CLASSES.CLOSE_TAB} ${styles.closeTab}`}
+                                    type="close"
+                                    title={I18n.t( 'close-tab' )}
+                                    aria-label={I18n.t( 'aria.close-tab' )}
+                                    onClick={( event ) => {
+                                        this.handleTabClose( tabData, event );
+                                    }}
+                                />
+                            </Col>
+                        </Row>
+                    </div>
+                );
             }
-            if ( tab.isClosed ) {
-                return;
-            }
-            const { isActiveTab } = tab;
-            let tabStyleClass = styles.tab;
-            const tabData = {
-                key: tab.index,
-                tabIndex: tab.index,
-                url: tab.url
-            };
-            if ( isActiveTab ) {
-                tabStyleClass = `${styles.activeTab} ${CLASSES.ACTIVE_TAB}`;
-            }
-            return (
-                <div
-                    key={tab.index}
-                    className={`${tabStyleClass} ${CLASSES.TAB}`}
-                    onClick={(event) => {
-                        this.handleTabClick( tabData, event );
-                    }}
-                >
-                    <Row align="middle" justify="space-between" type="flex">
-                        <Col>
-                            <div className={styles.faviconContainer}>
-                                {tab.isLoading && (
-                                    <Icon type="loading" className={styles.loadingIcon} />
-                                )}
-                                {!tab.isLoading && tab.favicon && (
-                                    <img
-                                        alt=""
-                                        className={styles.favicon}
-                                        id="favicon-img"
-                                        src={tab.favicon}
-                                    />
-                                )}
-                            </div>
-                        </Col>
-                        <Col className={styles.tabText}>{title || 'New Tab'}</Col>
-                        <Col>
-                            <Icon
-                                className={`${CLASSES.CLOSE_TAB} ${styles.closeTab}`}
-                                type="close"
-                                title={I18n.t( 'close-tab' )}
-                                aria-label={I18n.t( 'aria.close-tab' )}
-                                onClick={( event ) => {
-                                    this.handleTabClose( tabData , event );
-                                }}
-                            />
-                        </Col>
-                    </Row>
-                </div>
-            );
-        } );
+        );
     };
 
     handleAddTabClick( event ) {
