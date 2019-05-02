9b38a18418ccd1701a1774afb8f06bc963953fc2
joshuef <joshuef@gmail.com>
2019-05-02 09:03:51 +0200

    fix(Invite): Invite page not notified as blocked when opening in clearnet browser (#699)

--- app/extensions/safe/blockNonSafeReqs.ts
+++ app/extensions/safe/blockNonSafeReqs.ts
@@ -17,41 +17,48 @@ export const blockNonSAFERequests = () => {
 
     const safeSession = remote.session.fromPartition( CONFIG.SAFE_PARTITION );
 
-    safeSession.webRequest.onBeforeRequest( filter, ( details, callback ) => {
-    //  testcafe needs access to inject code
-        if ( isRunningTestCafeProcess ) {
-            callback( {} );
-            return;
-        }
+    safeSession.webRequest.onBeforeRequest(
+        filter,
+        ( details, callback ): void => {
+            //  testcafe needs access to inject code
+            if ( isRunningTestCafeProcess ) {
+                callback( {} );
+                return;
+            }
 
-        if ( urlIsAllowedBySafe( details.url ) ) {
-            logger.info( `Allowing url ${details.url}` );
-            callback( {} );
-            return;
-        }
+            if ( urlIsAllowedBySafe( details.url ) ) {
+                logger.info( `Allowing url ${details.url}` );
+                callback( {} );
+                return;
+            }
 
-        // HACK for idMgr and Patter. until:
-        // https://github.com/parcel-bundler/parcel/issues/1663
-        if ( details.url.includes( 'font_148784_v4ggb6wrjmkotj4i' ) ) {
-            const thePath = parseURL( details.url ).path;
-            const ext = path.extname( thePath );
+            // HACK for idMgr and Patter. until:
+            // https://github.com/parcel-bundler/parcel/issues/1663
+            if ( details.url.includes( 'font_148784_v4ggb6wrjmkotj4i' ) ) {
+                const thePath = parseURL( details.url ).path;
+                const extension = path.extname( thePath );
 
-            const newUrl = `http://localhost:${CONFIG.PORT}/dummy/iconfont${ext}`;
-            callback( { redirectURL: newUrl } );
-            return;
-        }
+                const newUrl = `http://localhost:${
+                    CONFIG.PORT
+                }/dummy/iconfont${extension}`;
+                callback( { redirectURL: newUrl } );
+                return;
+            }
 
-        if ( httpRegExp.test( details.url ) ) {
-            if ( allowedHttp.includes( details.url ) ) {
-                try {
-                    open( details.url );
-                } catch ( e ) {
-                    logger.error( e );
+            if ( httpRegExp.test( details.url ) ) {
+                if ( allowedHttp.includes( details.url ) ) {
+                    try {
+                        open( details.url );
+                        callback( { redirectURL: 'about:blank' } );
+                        return;
+                    } catch ( error ) {
+                        logger.error( error );
+                    }
                 }
             }
-        }
 
-        logger.error( 'Blocked URL:', details.url );
-        callback( { cancel: true } );
-    } );
+            logger.error( 'Blocked URL:', details.url );
+            callback( { cancel: true } );
+        }
+    );
 };
