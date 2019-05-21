98050d167a682061c9e4c71b5481f53efaaaca2d
Manav Chhabria <manavbp@gmail.com>
2019-05-21 12:32:25 +0530

    chore(commit): fix lint issue

--- app/components/Tab/Tab.tsx
+++ app/components/Tab/Tab.tsx
@@ -627,7 +627,7 @@ For updates or to submit ideas and suggestions, visit https://github.com/maidsaf
           }
           webIdUpdater();
       `;
-        webview.executeJavaScript( setupEventEmitter );
+        if ( webview !== null ) webview.executeJavaScript( setupEventEmitter );
     };
 
     setCurrentWebId( newWebId ) {
