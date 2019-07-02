0b74d251cb253e5321866d79b476d1a015c53e39
Manav Chhabria <manavbp@gmail.com>
2019-07-02 15:04:17 +0530

    fix(test): add testcafe tests for addTabNext

--- __testcafe__/helpers.js
+++ __testcafe__/helpers.js
@@ -10,8 +10,14 @@ export const getPageUrl = ClientFunction( () => window.location.href );
 export const resetStore = async ( t ) => {
     await clickOnMainMenuItem( ['&Tests', 'Reset the store'] );
 };
+export const addTabNext = async ( t ) => {
+    await clickOnMainMenuItem( ['&Tests', 'Add Tab Next'] );
+};
 export const openLocation = async ( t ) => {
     await clickOnMainMenuItem( ['&File', 'Open Location'] );
+};
+export const selectPreviousTab = async ( t ) => {
+    await clickOnMainMenuItem( ['&File', 'Select Previous Tab'] );
 };
 
 export const getPageTitle = ClientFunction( () => document.title );
