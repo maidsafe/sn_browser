1dd21b20855d7d9c79116ecfb2793210e146a8e9
Josh Wilson <joshuef@gmail.com>
2019-10-22 20:01:01 +0200

    tests(resolver): Add tests for NRS resolution w/ versions
    * Refactors the manageAndModifyRequests file, breaking it up to test
    individual logic parts.
    + Add tests for each of those parts.

--- app/extensions/safe/manageAndModifyRequests.ts
+++ app/extensions/safe/manageAndModifyRequests.ts
@@ -3,132 +3,161 @@ import { Store } from 'redux';
 import { remote } from 'electron';
 import { parse as parseURL } from 'url';
 import path from 'path';
-import { CONFIG, isRunningTestCafeProcess, allowedHttp } from '$Constants';
+import { CONFIG, isRunningTestCafeProcess } from '$Constants';
 import { logger } from '$Logger';
 import { urlIsValid } from '$Extensions';
+
+export const INVALID_URL = 'invalidUrl::cancel';
+export const shouldBlockRequestForPage = (
+    requestUrl: string,
+    pageUrl: string
+): boolean => {
+    //  testcafe needs access to inject code
+    const httpRegExp = new RegExp( '^http' );
+
+    if ( isRunningTestCafeProcess ) {
+        return false;
+    }
+
+    if ( requestUrl === INVALID_URL ) {
+        return true;
+    }
+    // first thing lets allow valid urls...
+    if ( !urlIsValid( requestUrl ) ) {
+        logger.info( `Blocking url ${requestUrl}` );
+        return true;
+    }
+
+    const parseQuery = true;
+    const parsedTabUrl = parseURL( pageUrl, parseQuery );
+    const requestedSiteParsed = parseURL( requestUrl, parseQuery );
+    const requestedSiteVersion = requestedSiteParsed.query.v;
+    // requesting unversioned resource from another page... which is not versioned block
+    if (
+        pageUrl.length > 0 &&
+    requestedSiteParsed.host !== parsedTabUrl.host &&
+    !requestedSiteVersion
+    ) {
+        logger.warn(
+            'Unversioned External Resource Request Blocked @ URL:',
+            requestUrl
+        );
+        return true;
+    }
+
+    return false;
+};
+
+export const getSourcePageUrl = (
+    details: { headers: { 'User-Agent': string } },
+    store: Store
+): string => {
+    const userAgent = details.headers['User-Agent'];
+    const targetWebContentsId = userAgent
+        ? parseInt( userAgent.split( 'webContentsId:' )[1], 10 )
+        : undefined;
+
+    // HACK, w/ ?v=x query params we need another way to get the current
+    // content version. So we use webContentsIds to do this.
+    const state = store.getState();
+    const { tabs } = state;
+
+    const tabsArray = Object.values( tabs );
+
+    let targetTab = null;
+    if ( tabsArray.length > 0 ) {
+        targetTab = tabsArray.find(
+            ( tab: { webContentsId: number; url: string } ) =>
+                tab.webContentsId && tab.webContentsId === targetWebContentsId
+        );
+    }
+
+    const url = targetTab ? targetTab.url : '';
+    return url;
+};
+export const redirectUrlIfNeeded = (
+    requestUrl: string,
+    appLocation: string,
+    platform?: string
+) => {
+    let redirectURL = requestUrl;
+    const parsedUrl = parseURL( requestUrl );
+    let theAppLocation = appLocation;
+    // MacOS, devmode. Attempts are made to load from
+    // /Users... electron...map.js
+    if ( platform === 'darwin' ) {
+        const theSplit = theAppLocation.split( '.app' );
+        theAppLocation = `${theSplit[0]}.app`;
+    }
+
+    if ( parsedUrl.path && parsedUrl.path.includes( appLocation ) ) {
+        const fileLocation = requestUrl.split( appLocation )[1];
+        redirectURL = `file://${appLocation}.app/${fileLocation}`;
+        logger.verbose( 'Permitting app dep url', redirectURL );
+
+        return redirectURL;
+    }
+
+    return redirectURL;
+};
+
+export const mapPageResourceToPageVersion = (
+    sourcePageUrl: string,
+    requestUrl: string
+): string => {
+    const parseQuery = true;
+    const parsedRequestUrl = parseURL( requestUrl, parseQuery );
+    const parsedTabUrl = parseURL( sourcePageUrl, parseQuery );
+    let versionedUrl = requestUrl;
+
+    // we need to check if the req comes from the same site...
+    // const requegstedSiteParsed = parseURL( parsedRequestUrl.path.slice( 1 ), parseQuery ); // remove localhost:port
+
+    const tabSiteVersion = parsedTabUrl.query.v;
+
+    const requestedSiteVersion = parsedRequestUrl.query
+        ? parsedRequestUrl.query.v
+        : null;
+
+    if (
+        parsedRequestUrl.host === parsedTabUrl.host &&
+    !parsedRequestUrl.query.v
+    ) {
+        logger.verbose(
+            'On a versioned site, updated resource req, to: ',
+            `${requestUrl}?v=${tabSiteVersion}`
+        );
+        versionedUrl = `${requestUrl}?v=${tabSiteVersion}`;
+    }
+    return versionedUrl;
+};
 
 export const manageAndModifyRequests = ( store: Store ) => {
     const filter = {
         urls: ['*://*/*']
     };
 
-    const httpRegExp = new RegExp( '^http' );
-
     const safeSession = remote.session.fromPartition( CONFIG.SAFE_PARTITION );
 
     safeSession.webRequest.onBeforeRequest( filter, ( details, callback ): void => {
-    //  testcafe needs access to inject code
-        if ( isRunningTestCafeProcess ) {
-            callback( {} );
-            return;
-        }
-        const userAgent = details.headers['User-Agent'];
-
-        const targetWebContentsId = userAgent
-            ? parseInt( userAgent.split( 'webContentsId:' )[1], 10 )
-            : undefined;
-
-        // HACK, w/ ?v=x query params we need another way to get the current
-        // content version. So we use webContentsIds to do this.
-        const state = store.getState();
-        const { tabs } = state;
-
-        const tabsArray = Object.values( tabs );
-
-        let targetUrl = details.url;
+    // First lets get the page URL for comparing request
 
         const parseQuery = true;
-        const parsedUrl = parseURL( targetUrl, parseQuery );
-        if ( tabsArray.length > 0 ) {
-            const targetTab = tabsArray.find(
-                ( tab: { webContentsId: number; url: string } ) =>
-                    tab.webContentsId && tab.webContentsId === targetWebContentsId
-            );
+        const sourcePageUrl = getSourcePageUrl( details, store );
 
-            if ( targetTab ) {
-                const targetVersion = targetTab.url;
+        let finalUrl = details.url;
 
-                const parsedTabUrl = parseURL( targetTab.url, parseQuery );
+        const parsedFullServerReq = parseURL( finalUrl );
+        const requestedSite = parsedFullServerReq.path.slice( 1 ); // remove localhost:port
 
-                // we need to check if the req comes from the same site...
-                const requestedSiteParsed = parseURL(
-                    parsedUrl.path.slice( 1 ),
-                    parseQuery
-                ); // remove localhost:port
-                const tabSiteVersion = parsedTabUrl.query.v;
-                const requestedSiteVersion = requestedSiteParsed.query
-                    ? requestedSiteParsed.query.v
-                    : null;
-                if (
-                    requestedSiteParsed.host === parsedTabUrl.host &&
-          !parsedUrl.query.v
-                ) {
-                    logger.verbose(
-                        'On a versioned site, updated resource req, to: ',
-                        `${targetUrl}?v=${tabSiteVersion}`
-                    );
-                    targetUrl = `${targetUrl}?v=${tabSiteVersion}`;
-                }
+        const appLocation = remote.app.getPath( 'exe' );
 
-                // requesting unversioned resource from another page... which is not versioned block
-                if (
-                    requestedSiteParsed.host !== parsedTabUrl.host &&
-          !requestedSiteVersion
-                ) {
-                    logger.warn(
-                        'Unversioned External Resource Request Blocked @ URL:',
-                        targetUrl
-                    );
-                    callback( { cancel: true } );
-                    return;
-                }
-            }
+        finalUrl = mapPageResourceToPageVersion( sourcePageUrl, finalUrl );
+        finalUrl = redirectUrlIfNeeded( finalUrl, appLocation, process.platform );
+        if ( shouldBlockRequestForPage( finalUrl, sourcePageUrl ) ) {
+            callback( { cancel: true } );
+        } else {
+            callback( {} );
         }
-
-        // MacOS, devmode. Attempts are made to load from
-        // /Users... electron...map.js
-        let appLocation = remote.app.getPath( 'exe' );
-
-        if ( process.platform === 'darwin' ) {
-            const theSplit = appLocation.split( '.app' );
-            appLocation = `${theSplit[0]}.app`;
-        }
-
-        if ( parsedUrl.path && parsedUrl.path.includes( appLocation ) ) {
-            const fileLocation = targetUrl.split( appLocation )[1];
-            const redirectURL = `file://${appLocation}.app/${fileLocation}`;
-            logger.verbose( 'Permitting app dep url', redirectURL );
-            callback( { redirectURL } );
-
-            return;
-        }
-
-        if ( urlIsValid( targetUrl ) ) {
-            logger.info( `Allowing url ${targetUrl}` );
-
-            if ( details.url === targetUrl ) {
-                callback( {} );
-                return;
-            }
-
-            callback( { redirectURL: targetUrl } );
-
-            return;
-        }
-
-        if ( httpRegExp.test( targetUrl ) ) {
-            if ( allowedHttp.includes( targetUrl ) ) {
-                try {
-                    open( targetUrl );
-                    callback( { redirectURL: 'about:blank' } );
-                    return;
-                } catch ( error ) {
-                    logger.error( error );
-                }
-            }
-        }
-
-        logger.warn( 'Blocked URL:', targetUrl );
-        callback( { cancel: true } );
     } );
 };
---
+++ app/extensions/safe/test/utils/manageAndModify.spec.ts
@@ -0,0 +1,150 @@
+import {
+    shouldBlockRequestForPage,
+    getSourcePageUrl,
+    redirectUrlIfNeeded,
+    mapPageResourceToPageVersion,
+    INVALID_URL
+} from '$Extensions/safe/manageAndModifyRequests';
+
+describe( 'shouldBlockRequestForPage', () => {
+    it( 'exists', () => {
+        expect( shouldBlockRequestForPage ).not.toBeNull();
+    } );
+
+    it( 'blocks requests from another site w/o version', () => {
+        expect(
+            shouldBlockRequestForPage( 'safe://elsewhere/thing.png', 'safe://mysite' )
+        ).toBeTruthy();
+    } );
+
+    it( ' does not block requests from the browser itself', () => {
+        expect(
+            shouldBlockRequestForPage( 'safe://elsewhere/thing.png', '' )
+        ).toBeFalsy();
+    } );
+
+    it( 'blocks invalid URL reqs', () => {
+        expect(
+            shouldBlockRequestForPage( INVALID_URL, 'safe://mysite' )
+        ).toBeTruthy();
+    } );
+    it( 'allows requests from another site w/ version', () => {
+        expect(
+            shouldBlockRequestForPage(
+                'safe://elsewhere/thing.png?v=2',
+                'safe://mysite'
+            )
+        ).toBeFalsy();
+    } );
+
+    it( 'blocks http/s reqs', () => {
+        expect(
+            shouldBlockRequestForPage( 'http:blaaa', 'safe://mysite' )
+        ).toBeTruthy();
+        expect(
+            shouldBlockRequestForPage( 'http://blaaa', 'safe://mysite' )
+        ).toBeTruthy();
+        expect(
+            shouldBlockRequestForPage( 'https:blaaa', 'safe://mysite' )
+        ).toBeTruthy();
+        expect(
+            shouldBlockRequestForPage( 'https://blaaa', 'safe://mysite' )
+        ).toBeTruthy();
+    } );
+} );
+
+describe( 'getSourcePageUrl', () => {
+    it( 'should return the page url when same webId is found', () => {
+        const mockStore = {
+            getState: jest.fn( () => ( {
+                tabs: {
+                    aTab: {
+                        webContentsId: 22,
+                        url: 'safe://mysite'
+                    }
+                }
+            } ) )
+        };
+
+        const mockDetails = {
+            headers: {
+                'User-Agent': 'blabal; webContentsId: 22'
+            }
+        };
+
+        expect( getSourcePageUrl( mockDetails, mockStore ) ).toBe( 'safe://mysite' );
+    } );
+    it( 'should return an empty string when no page w/ webId is found', () => {
+        const mockStore = {
+            getState: jest.fn( () => ( {
+                tabs: {
+                    aTab: {
+                        webContentsId: 22,
+                        url: 'safe://mysite'
+                    }
+                }
+            } ) )
+        };
+
+        const mockDetails = {
+            headers: {
+                'User-Agent': 'blabal; webContentsId: 44'
+            }
+        };
+
+        expect( getSourcePageUrl( mockDetails, mockStore ) ).toBe( '' );
+    } );
+} );
+
+describe( 'redirectUrlIfNeeded', () => {
+    it( 'should redirect to file if .app is present in req url', () => {
+        expect( redirectUrlIfNeeded ).not.toBeNull();
+
+        const appUrl = 'osLocation.app/bla/blablabla';
+        expect( redirectUrlIfNeeded( appUrl, 'osLocation.app', 'darwin' ) ).toMatch(
+            'file://'
+        );
+        expect( redirectUrlIfNeeded( appUrl, 'osLocation.app', 'darwin' ) ).toMatch(
+            '.app'
+        );
+    } );
+
+    it( 'should return the same url if standard safe', () => {
+        expect( redirectUrlIfNeeded( 'safe://hithere', 'osLocation.app' ) ).toEqual(
+            'safe://hithere'
+        );
+        expect(
+            redirectUrlIfNeeded( 'safe://hithere', 'osLocation.app', 'darwin' )
+        ).toEqual( 'safe://hithere' );
+    } );
+} );
+
+describe( 'mapPageResourceToPageVersion', () => {
+    it( 'should not change version on a versioned site', () => {
+        expect(
+            mapPageResourceToPageVersion(
+                'safe://mysite?v=102',
+                'safe://mysite/jpg?v=100'
+            )
+        ).toEqual( 'safe://mysite/jpg?v=100' );
+    } );
+
+    it( 'should change version of an unversioned resource when same site', () => {
+        expect(
+            mapPageResourceToPageVersion(
+                'safe://mysite?v=2',
+                'safe://mysite/css/bla.jpg'
+            )
+        ).toEqual( 'safe://mysite/css/bla.jpg?v=2' );
+    } );
+
+    // it('should change version of an unversioned resource when same site', () => {
+    // expect(mapPageResourceToPageVersion('safe://mysite?v=2', 'safe://mysite/jpg'))
+    // .toEqual('safe://mysite/jpg?v=2')
+    // })
+    it( 'should not change version of an unversioned resource form a different site', () => {
+        expect(
+            mapPageResourceToPageVersion( 'safe://mysite?v=2', 'safe://nowhere/jpg' )
+        ).toEqual( 'safe://nowhere/jpg' );
+    } );
+} );
