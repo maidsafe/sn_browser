d6072ae02e2ec1a866d9b6742066025c6be7e725
Manav Chhabria <manavbp@gmail.com>
2019-10-22 15:52:47 +0530

    chore(refactor): Add SNAPP update logic to browser

--- app/autoUpdate.ts
+++ app/autoUpdate.ts
@@ -3,6 +3,8 @@ import log from 'electron-log';
 import { dialog } from 'electron';
 import { logger } from '$Logger';
 import * as notificationActions from '$Actions/notification_actions';
+import { shouldTriggerForceUpdate } from '$Constants';
+import { openSnappWithArgs } from './snappHelperFuncs';
 
 autoUpdater.autoDownload = false;
 
@@ -16,130 +18,151 @@ const clearNotification = ( payload: { id: string } ) =>
 
 // Thow error incase there is an issue in updating
 autoUpdater.on( 'error', ( error ) => {
+    if ( process.platform === 'linux' || process.platform === 'win32' ) {
+        openSnappWithArgs( [
+            `--update-failed`,
+            `--appId:safe.browser`,
+            `--error:${error}`
+        ] );
+    } else {
+        openSnappWithArgs( `--update-failed --appId:safe.browser --error:${error}` );
+    }
     dialog.showErrorBox(
-        'Error: ',
+        'There was an issue updating SAFE Browser. The update failed due to the following error:',
         error == null ? 'unknown' : ( error.stack || error ).toString()
     );
 } );
 
 // Check for update and ask if user wants to download it
 autoUpdater.on( 'update-available', () => {
-    const notificationId = Math.random().toString( 36 );
-    const title = 'Update SAFE BROWSER';
-    const body = 'Download Latest Version?';
+    if ( !shouldTriggerForceUpdate ) {
+        const notificationId = Math.random().toString( 36 );
+        const title = 'Update SAFE BROWSER';
+        const body = 'Download Latest Version?';
 
-    const ignoreRequest = () => {
-        logger.info( 'replace these ipcRenderer.send calls' );
-        clearNotification( { id: notificationId } );
-    };
+        const ignoreRequest = () => {
+            logger.info( 'replace these ipcRenderer.send calls' );
+            clearNotification( { id: notificationId } );
+        };
 
-    const success = () => {
+        const success = () => {
+            autoUpdater.downloadUpdate();
+            clearNotification( { id: notificationId } );
+        };
+
+        const denial = () => {
+            logger.info( 'Denied downloading update' );
+            clearNotification( { id: notificationId } );
+        };
+
+        const theNotification = {
+            id: notificationId,
+            type: 'warning',
+            body,
+            isPrompt: true,
+            title,
+            duration: 0
+        };
+        const responseMap = {
+            allow: success,
+            deny: denial,
+            ignore: ignoreRequest
+        };
+
+        addNotification( theNotification );
+
+        const stopListening = store.subscribe( () => {
+            logger.info( 'Listener for updateBrowserNotification' );
+
+            const state = store.getState();
+            const { notifications } = state;
+
+            if ( !notifications ) {
+                return;
+            }
+
+            const ourNotification = notifications.find(
+                ( n ) => n.id === notificationId
+            );
+
+            if ( !ourNotification || ourNotification === theNotification ) {
+                return;
+            }
+
+            if ( ourNotification.response && responseMap[ourNotification.response] ) {
+                responseMap[ourNotification.response]();
+                stopListening();
+            }
+        } );
+    } else {
         autoUpdater.downloadUpdate();
-        clearNotification( { id: notificationId } );
-    };
-
-    const denial = () => {
-        logger.info( 'Denied downloading update' );
-        clearNotification( { id: notificationId } );
-    };
-
-    const theNotification = {
-        id: notificationId,
-        type: 'warning',
-        body,
-        isPrompt: true,
-        title,
-        duration: 0
-    };
-    const responseMap = {
-        allow: success,
-        deny: denial,
-        ignore: ignoreRequest
-    };
-
-    addNotification( theNotification );
-
-    const stopListening = store.subscribe( () => {
-        logger.info( 'Listener for updateBrowserNotification' );
-
-        const state = store.getState();
-        const { notifications } = state;
-
-        if ( !notifications ) {
-            return;
-        }
-
-        const ourNotification = notifications.find( ( n ) => n.id === notificationId );
-
-        if ( !ourNotification || ourNotification === theNotification ) {
-            return;
-        }
-
-        if ( ourNotification.response && responseMap[ourNotification.response] ) {
-            responseMap[ourNotification.response]();
-            stopListening();
-        }
-    } );
+    }
 } );
 
 autoUpdater.on( 'update-downloaded', () => {
-    const notificationId = Math.random().toString( 36 );
-    const title = 'Update Downloaded';
-    const message = 'Update ready to be Installed';
+    if ( !shouldTriggerForceUpdate ) {
+        const notificationId = Math.random().toString( 36 );
+        const title = 'Update Downloaded';
+        const message = 'Update ready to be Installed';
 
-    const ignoreRequest = () => {
-        logger.info( 'replace these ipcRenderer.send calls' );
-        clearNotification( { id: notificationId } );
-    };
+        const ignoreRequest = () => {
+            logger.info( 'replace these ipcRenderer.send calls' );
+            clearNotification( { id: notificationId } );
+        };
 
-    const success = () => {
+        const success = () => {
+            autoUpdater.quitAndInstall();
+            clearNotification( { id: notificationId } );
+        };
+
+        const denial = () => {
+            logger.info( 'Update denied' );
+            clearNotification( { id: notificationId } );
+        };
+
+        const theNotification = {
+            id: notificationId,
+            type: 'warning',
+            isPrompt: true,
+            title,
+            body: message,
+            duration: 0
+        };
+
+        const responseMap = {
+            allow: success,
+            deny: denial,
+            ignore: ignoreRequest
+        };
+
+        addNotification( theNotification );
+
+        const stopListening = store.subscribe( () => {
+            logger.info( 'Listener for updateBrowserNotification' );
+
+            const state = store.getState();
+            const { notifications } = state;
+
+            if ( !notifications ) {
+                return;
+            }
+
+            const ourNotification = notifications.find(
+                ( n ) => n.id === notificationId
+            );
+
+            if ( !ourNotification || ourNotification === theNotification ) {
+                return;
+            }
+
+            if ( ourNotification.response && responseMap[ourNotification.response] ) {
+                responseMap[ourNotification.response]();
+                stopListening();
+            }
+        } );
+    } else {
         autoUpdater.quitAndInstall();
-        clearNotification( { id: notificationId } );
-    };
-
-    const denial = () => {
-        logger.info( 'Update denied' );
-        clearNotification( { id: notificationId } );
-    };
-
-    const theNotification = {
-        id: notificationId,
-        type: 'warning',
-        isPrompt: true,
-        title,
-        body: message,
-        duration: 0
-    };
-
-    const responseMap = {
-        allow: success,
-        deny: denial,
-        ignore: ignoreRequest
-    };
-
-    addNotification( theNotification );
-
-    const stopListening = store.subscribe( () => {
-        logger.info( 'Listener for updateBrowserNotification' );
-
-        const state = store.getState();
-        const { notifications } = state;
-
-        if ( !notifications ) {
-            return;
-        }
-
-        const ourNotification = notifications.find( ( n ) => n.id === notificationId );
-
-        if ( !ourNotification || ourNotification === theNotification ) {
-            return;
-        }
-
-        if ( ourNotification.response && responseMap[ourNotification.response] ) {
-            responseMap[ourNotification.response]();
-            stopListening();
-        }
-    } );
+    }
 } );
 
 export class AppUpdater {
@@ -147,7 +170,10 @@ export class AppUpdater {
         log.transports.file.level = 'info';
         autoUpdater.logger = log;
         store = passedStore;
+    }
 
+    // eslint-disable-next-line class-methods-use-this
+    checkForUpdate() {
         try {
             autoUpdater.checkForUpdatesAndNotify();
         } catch ( error ) {
