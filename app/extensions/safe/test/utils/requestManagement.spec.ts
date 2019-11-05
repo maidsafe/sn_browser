2bbfdbf49026c43405bef0bda3e728e26cf9dc6c
Josh Wilson <joshuef@gmail.com>
2019-11-05 11:28:37 +0100

    fix(resolution): Pweb resolution tweaks.
    update tests to better clarify request handling.
    
    For #1061

---
+++ app/extensions/safe/test/utils/requestManagement.spec.ts
@@ -0,0 +1,237 @@
+import {
+    shouldBlockRequestForPage,
+    getSourcePageUrl,
+    redirectUrlIfNeeded,
+    manageAndModifyRequest,
+    mapPageResourceToPageVersion,
+    INVALID_URL
+} from '$Extensions/safe/requestManagement';
+
+const SERVER = 'http://localhost:55155';
+
+jest.mock( 'electron', () => {
+    const app = {
+        getPath: jest.fn( () => {
+            return '/Somewhere/Electron.app';
+        } )
+    };
+
+    return {
+        require: jest.fn(),
+        match: jest.fn(),
+        app,
+        remote: {
+            process: {
+                execPath: process.execPath
+            },
+            getGlobal: jest.fn(),
+            app
+        },
+        dialog: jest.fn()
+    };
+} );
+
+describe( 'shouldBlockRequestForPage', () => {
+    it( 'exists', () => {
+        expect( shouldBlockRequestForPage ).not.toBeNull();
+    } );
+
+    it( 'blocks requests from another site w/o version', () => {
+        expect(
+            shouldBlockRequestForPage( 'safe://elsewhere/thing.png', 'safe://mysite' )
+        ).toBeTruthy();
+    } );
+
+    it( ' does not block requests from the browser itself', () => {
+        expect(
+            shouldBlockRequestForPage( 'https://localhost:3132/thing.png', '' )
+        ).toBeFalsy();
+    } );
+
+    it( 'blocks invalid URL reqs', () => {
+        expect(
+            shouldBlockRequestForPage( INVALID_URL, 'safe://mysite' )
+        ).toBeTruthy();
+    } );
+    it( 'allows requests from another site w/ version', () => {
+        expect(
+            shouldBlockRequestForPage(
+                'safe://elsewhere/thing.png?v=2',
+                'safe://mysite'
+            )
+        ).toBeFalsy();
+    } );
+
+    it( 'blocks http/s reqs', () => {
+        expect(
+            shouldBlockRequestForPage( 'http:blaaa', 'safe://mysite' )
+        ).toBeTruthy();
+        expect(
+            shouldBlockRequestForPage( 'http://blaaa', 'safe://mysite' )
+        ).toBeTruthy();
+        expect(
+            shouldBlockRequestForPage( 'https:blaaa', 'safe://mysite' )
+        ).toBeTruthy();
+        expect(
+            shouldBlockRequestForPage( 'https://blaaa', 'safe://mysite' )
+        ).toBeTruthy();
+    } );
+} );
+
+describe( 'getSourcePageUrl', () => {
+    it( 'should return the page url when same webId is found', () => {
+        const mockStore = {
+            getState: jest.fn( () => ( {
+                tabs: {
+                    aTab: {
+                        webContentsId: 22,
+                        url: 'safe://mysite'
+                    }
+                }
+            } ) )
+        };
+
+        const mockDetails = {
+            headers: {
+                'User-Agent': 'blabal; webContentsId: 22'
+            }
+        };
+
+        expect( getSourcePageUrl( mockDetails, mockStore ) ).toBe( 'safe://mysite' );
+    } );
+    it( 'should return an empty string when no page w/ webId is found', () => {
+        const mockStore = {
+            getState: jest.fn( () => ( {
+                tabs: {
+                    aTab: {
+                        webContentsId: 22,
+                        url: 'safe://mysite'
+                    }
+                }
+            } ) )
+        };
+
+        const mockDetails = {
+            headers: {
+                'User-Agent': 'blabal; webContentsId: 44'
+            }
+        };
+
+        expect( getSourcePageUrl( mockDetails, mockStore ) ).toBe( '' );
+    } );
+} );
+
+describe( 'redirectUrlIfNeeded', () => {
+    it( 'should redirect to file if .app is present in req url', () => {
+        expect( redirectUrlIfNeeded ).not.toBeNull();
+
+        const appUrl =
+      'safe://dweb/Users/josh/Projects/safe/forks/browser/node_modules/electron/dist/Electron.app/Contents/Resources/electron.asar/browser/api/module-list.js.map';
+        expect( redirectUrlIfNeeded( appUrl ) ).toMatchObject( {
+            shouldRedirect: true,
+            redirectURL:
+        'file:///Somewhere/Electron.app/Contents/Resources/electron.asar/browser/api/module-list.js.map'
+        } );
+    } );
+
+    it( 'should return the same url if standard safe', () => {
+        expect( redirectUrlIfNeeded( 'safe://hithere' ) ).toMatchObject( {
+            shouldRedirect: false
+        } );
+    } );
+} );
+
+describe( 'mapPageResourceToPageVersion', () => {
+    it( 'should not change version on a versioned site', () => {
+        expect(
+            mapPageResourceToPageVersion(
+                'safe://mysite?v=102',
+                'safe://mysite/jpg?v=100'
+            )
+        ).toEqual( 'safe://mysite/jpg?v=100' );
+    } );
+
+    it( 'should change version of an unversioned resource when same site and thats versioned ', () => {
+        expect(
+            mapPageResourceToPageVersion(
+                'safe://mysite?v=2',
+                'safe://mysite/css/bla.jpg'
+            )
+        ).toEqual( 'safe://mysite/css/bla.jpg?v=2' );
+    } );
+
+    it( 'should change version of an unversioned resource when on unversioned same site', () => {
+        expect(
+            mapPageResourceToPageVersion( 'safe://mysite', 'safe://mysite/css/bla.jpg' )
+        ).toEqual( 'safe://mysite/css/bla.jpg' );
+    } );
+
+    it( 'should not change version of an unversioned resource form a different site', () => {
+        expect(
+            mapPageResourceToPageVersion( 'safe://mysite?v=2', 'safe://nowhere.jpg' )
+        ).toEqual( 'safe://nowhere.jpg' );
+    } );
+} );
+
+// integration of all of the above...
+describe( 'manageAndModifyRequest', () => {
+    const store = {
+        getState: jest.fn( () => ( {
+            tabs: {
+                someTab: {
+                    webContentsId: 1,
+                    url: 'safe://same'
+                }
+            }
+        } ) )
+    };
+    let cb = jest.fn();
+    const details = {
+        url: `${SERVER}/safe://same/main.42ea068a.js`,
+        headers: {
+            'User-Agent': 'blabla; webContentsId: 1'
+        }
+    };
+
+    beforeEach( () => {
+        cb = jest.fn();
+    } );
+
+    it( 'should allow javascript files from same site', () => {
+        manageAndModifyRequest( details, cb, store );
+        expect( cb ).not.toHaveBeenCalledWith( { cancel: true } );
+        expect( cb ).not.toHaveBeenCalledWith( { redirect: true } );
+    } );
+
+    it( 'should allow css files from same site', () => {
+        details.url = `${SERVER}/safe://same/main.bla.css`;
+
+        manageAndModifyRequest( details, cb, store );
+        expect( cb ).not.toHaveBeenCalledWith( { cancel: true } );
+        expect( cb ).not.toHaveBeenCalledWith( { redirect: true } );
+    } );
+
+    it( 'should block css files from different site', () => {
+        details.url = `${SERVER}/safe://other/main.bla.js`;
+        manageAndModifyRequest( details, cb, store );
+        expect( cb ).toHaveBeenCalledWith( { cancel: true } );
+        expect( cb ).not.toHaveBeenCalledWith( { redirect: true } );
+    } );
+    it( 'should block javascript files from different site', () => {
+        details.url = `${SERVER}/safe://other/main.bla.css`;
+        manageAndModifyRequest( details, cb, store );
+        expect( cb ).toHaveBeenCalledWith( { cancel: true } );
+        expect( cb ).not.toHaveBeenCalledWith( { redirect: true } );
+    } );
+
+    it( 'should redirect app url', () => {
+        details.url = `${SERVER}/safe://dweb/Users/josh/Projects/safe/forks/browser/node_modules/electron/dist/Electron.app/Contents/Resources/electron.asar/browser/api/module-list.js.map`;
+
+        manageAndModifyRequest( details, cb, store );
+        expect( cb ).not.toHaveBeenCalledWith( { cancel: true } );
+        expect( cb ).toHaveBeenCalledWith( {
+            redirectURL:
+        'file:///Somewhere/Electron.app/Contents/Resources/electron.asar/browser/api/module-list.js.map'
+        } );
+    } );
+} );
--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,6 @@ module.exports = {
     moduleNameMapper: {
         '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
             '<rootDir>/mocks/fileMock.ts',
-        '\\.(css|less|scss)$': '<rootDir>/mocks/fileMock.ts',
-        '^spectron-lib(.*)$': '<rootDir>/__e2e__/lib$1'
+        '\\.(css|less|scss)$': '<rootDir>/mocks/fileMock.ts'
     }
 };
